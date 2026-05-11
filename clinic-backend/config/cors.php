0411611676dd873026674d81b447cf42cb1c5dd7
Lana Rammal <lanarammal75@gmail.com>
2026-05-12 01:12:50 +0300

    Fix local frontend backend connection

--- clinic-backend/config/cors.php
+++ clinic-backend/config/cors.php
@@ -17,7 +17,14 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:8081', 'http://localhost:8082'],
+'allowed_origins' => [
+    'http://localhost:8080',
+    'http://localhost:8081',
+    'http://localhost:8082',
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:8081',
+    'http://127.0.0.1:8082',
+],
 'allowed_origins_patterns' => [],
 'allowed_headers' => ['*'],
 'supports_credentials' => false,
